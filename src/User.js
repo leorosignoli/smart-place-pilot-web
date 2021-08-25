38e305b623a92d0c11813e20f21addecfcc3bfc1
leorosignoli <leonardo.andrade6@hotmail.com>
2021-08-25 18:00:45 -0300

    Fetch BackEnd
    Begins connection from REACT to JAVA

--- src/User.js
+++ src/User.js
@@ -6,20 +6,34 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+
+
 function User() {
 
   window.onload = function () {
+
     var userForm = document.getElementById('userForm');
     userForm.addEventListener('submit', function (event) {
+      event.preventDefault();
       var formData = new FormData(userForm);
       var object = {};
       formData.forEach(function (value, key) {
         object[key] = value;
       });
       var json = JSON.stringify(object);
-      console.log(json);
-      event.preventDefault(); //this func stops the form from submiting
+
+      fetch('http://localhost:8080/smart-place-pilot-rs/api/user/', {
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          "Content-Type": "application/json"
+        },
+        method:'POST',
+        body: json
+      })
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
     })
+
   }
 
   return (
@@ -35,31 +49,24 @@ function User() {
 
           <Form.Group className="mb-3">
             <Form.Label> Username </Form.Label>
-            <Form.Control placeholder="Username123"name="username" required/>
+            <Form.Control placeholder="Username123" name="username" required />
           </Form.Group>
 
           <Row className="mb-3">
             <Form.Group as={Col}>
               <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" name="first_name" required/>
+              <Form.Control type="text" name="firstName" required />
             </Form.Group>
             <Form.Group as={Col}>
               <Form.Label>Last Name</Form.Label>
-              <Form.Control type="text" name="last_name"  required/>
+              <Form.Control type="text" name="lastName" required />
             </Form.Group>
           </Row>
 
-
-          <Form.Label>E-mail</Form.Label>
-          <Form.Control name="email" required/>
-
-          <Row className="mb-3">
-            <Col>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" name="password"  required/>
-              <Form.Text className="text-muted">Password must contain at least 7 characters.</Form.Text>
-            </Col>
-          </Row>
+          <Form.Group>
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control name="email" className="mb-4" required />
+          </Form.Group>
 
           <Button variant="outline-dark" type="submit" >Register</Button>
         </Form>
