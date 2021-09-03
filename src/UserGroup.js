d09b6000289a3875e07f24abd59ac2fe9d20e95e
leorosignoli <leonardo.andrade6@hotmail.com>
2021-09-03 17:51:04 -0300

    Changes userGroup form submition
    Tries to implement userGroup form submission via PUT request, still needs to implement a way to get dropdown data from the database and append instead of remake.

--- src/UserGroup.js
+++ src/UserGroup.js
@@ -1,16 +1,14 @@
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Card } from 'react-bootstrap';
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
 import Button from 'react-bootstrap/Button';
 import './App.css';
 import Form from 'react-bootstrap/Form';
 
+//https://reactjs.org/docs/forms.html
+//this documentation says how to implement the dropdown correctly
 
-function UserGroup() {
-
-    //get dropdown data for USERS
+/*    //get dropdown data for USERS
     const [result, getdata] = useState([]);
     useEffect(() => {
         fetch('http://localhost:8080/smart-place-pilot-rs/api/user/list', {
@@ -27,111 +25,86 @@ function UserGroup() {
     const Ddlhandle = (e) => {
         setdata(e);
     }
+*/
 
-    //get dropdown data for GROUPS
-    const [result2, getdata2] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:8080/smart-place-pilot-rs/api/group/list', {
-            method: 'GET',
+class UserGroup extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { value: 'NoUser' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value : event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        var object = {};
+
+        object[0] = event.target[1].value;
+        var manualJson= ("{\"userGroups\":\""+event.target[1].value+"\" }");
+        console.log(manualJson);
+        fetch(`http://localhost:8080/smart-place-pilot-rs/api/user/${event.target[0].value}`, {
             headers: {
-                'Content-type': 'application/json'
-            }
+                'Content-Type': 'application/json',
+            },
+            method: 'PUT',
+            body: manualJson
         })
-            .then(resp2 => resp2.json())
-            .then(resp2 => getdata2(resp2))
-    }, [])
+            .then(response => console.log('Success:', JSON.stringify(response)))
+            .catch(error => console.error('Error:', error));
 
-    const [output2, setdata2] = useState([]);
-    const Ddlhandle2 = (e2) => {
-        setdata2(e2);
     }
 
+    render() {
+        return (
 
-    //send data to BackEnd
-    window.onload = function () {
-        var userForm = document.getElementById('usgrForm');
-        
-        userForm.addEventListener('submit', function (event) {
-            event.preventDefault();
-            var formData = new FormData(userForm);
+            <Card style={{ width: '30rem' }} id='card-control'>
+                <Card.Title className="text-center mt-3 mb-4">
+                    <h3>User Registration in a Group</h3>
+                </Card.Title>
+                <Card.Body>
 
-            var object = {};
-            formData.forEach(function (value, key) {
-                object[key] = value;
-            });
-            var json = JSON.stringify(object);
-            console.log(json);
+                    <Form id="userForm" onSubmit={this.handleSubmit}>
 
-            //gets the id to be put on url
-            for (var value of formData.values()) {
-                console.log(value);
-            }
-            for (var key of formData.keys()) {
-                console.log(key);
-            }
-            const id = formData.get('id');
+                        <Form.Group className="mb-3">
+                            <Form.Label>
+                                User Name
+                            </Form.Label>
+                            <select value={this.state.value} onChange={this.handleChange}>
+                                <option value="grapefruit">Grapefruit</option>
+                                <option value="lime">Lime</option>
+                                <option value="coconut">Coconut</option>
+                                <option value="mango">Mango</option>
+                            </select>
+                        </Form.Group>
 
-            fetch(`http://localhost:8080/smart-place-pilot-rs/api/user/${id}`, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                method: 'PUT',
-                body: json
-            })
-                .then(response => console.log('Success:', JSON.stringify(response)))
-                .catch(error => console.error('Error:', error));
-        })
+                        <Form.Group className="mb-3">
+                            <Form.Label>
+                                Group Name
+                            </Form.Label>
+
+                            <select value={this.state.value} onChange={this.handleChange}>
+                                <option value="grapefruit">Grapefruit</option>
+                                <option value="lime">Lime</option>
+                                <option value="coconut">Coconut</option>
+                                <option value="mango">Mango</option>
+                            </select>
+                        </Form.Group>
+
+                        <Button variant="outline-dark" type="submit" value="submit">
+                            Register relationship
+                        </Button>
+
+                    </Form>
+
+                </Card.Body>
+            </Card>
+
+        )
     }
-
-    return (
-
-        <Card style={{ width: '30rem' }} id='card-control'>
-            <Card.Title className="text-center mt-3 mb-4">
-                <h3>User Registration in a Group</h3>
-            </Card.Title>
-            <Card.Body>
-
-                <Form id="usgrForm" >
-
-                    <Form.Group className="mb-3">
-                        <Form.Label>
-                            User Name
-                        </Form.Label>
-                        <DropdownButton variant="outline-dark" id="dropdown-basic-button" title={output} >
-                            {
-                                result.map((items) => {
-                                    return (
-                                        <Dropdown.Item name="id" eventKey={items.id} value={items.id} onSelect={Ddlhandle} >{items.username} </Dropdown.Item>
-                                    )
-                                })
-                            }
-                        </DropdownButton>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Form.Label>
-                            Group Name
-                        </Form.Label>
-                        <DropdownButton variant="outline-dark" id="dropdown-basic-button" title={output2}>
-                            {
-                                result2.map((items2) => {
-                                    return (
-                                        <Dropdown.Item eventKey={items2.id} value={items2.id} onSelect={Ddlhandle2}  >{items2.name}</Dropdown.Item>
-                                    )
-                                })
-                            }
-                        </DropdownButton>
-                    </Form.Group>
-
-                    <Button variant="outline-dark" type="submit">
-                        Register relationship
-                    </Button>
-
-                </Form>
-
-            </Card.Body>
-        </Card>
-    )
 }
-
 export default UserGroup;
